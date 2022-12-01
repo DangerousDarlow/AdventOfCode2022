14f1a158e9d495caa0bcc86af13dd180c79f70b0
Nick Darlow <dangerous.darlow@gmail.com>
2022-12-01 18:43:11 +0000

    Solve day 1

---
+++ AdventOfCode2022/Day01.cs
@@ -0,0 +1,61 @@
+using FluentAssertions;
+
+namespace AdventOfCode2022;
+
+public class Day01
+{
+    [Theory]
+    [InlineData("Day01_Example.txt", 24000)]
+    [InlineData("Day01.txt", 74198)]
+    public async Task Part1(string inputPath, int result)
+    {
+        var input = await ReadInput(inputPath);
+        input
+            .CaloriesPerElf()
+            .Max()
+            .Should().Be(result);
+    }
+    
+    [Theory]
+    [InlineData("Day01_Example.txt", 45000)]
+    [InlineData("Day01.txt", 209914)]
+    public async Task Part2(string inputPath, int result)
+    {
+        var input = await ReadInput(inputPath);
+        input
+            .CaloriesPerElf()
+            .OrderDescending()
+            .Take(3)
+            .Aggregate((first, second) => first + second)
+            .Should().Be(result);
+    }
+
+    private static async Task<int?[]> ReadInput(string inputPath)
+    {
+        var lines = await File.ReadAllLinesAsync(inputPath);
+        return lines
+            .Select(s => int.TryParse(s, out var result) ? (int?) result : null)
+            .ToArray();
+    }
+}
+
+public static class Extensions
+{
+    public static IEnumerable<int> CaloriesPerElf(this IEnumerable<int?> values)
+    {
+        var sumForElf = 0;
+        foreach (var value in values)
+        {
+            if (value.HasValue)
+                sumForElf += value.Value;
+            else
+            {
+                var temp = sumForElf;
+                sumForElf = 0;
+                yield return temp;
+            }
+        }
+        
+        yield return sumForElf;
+    }
+}
